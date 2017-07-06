ede9ebfc8b1b87da38ea595cd016701a19d89e73
Michael Cowden <michael.cowden@gmail.com>
2017-07-06 06:09:50 -0400

    track ip

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
+    @user.ip_address = request.remote_ip
 
     if user_params[:email] =~ /163\.com$/
       flash[:notice] = "Nothing to see here"
