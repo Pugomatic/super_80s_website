b7af4c52ec3330224c9d7053b04f80137fe826e6
Michael Cowden <michael.cowden@gmail.com>
2017-08-29 13:58:46 -0400

    fix

--- app/controllers/admin/email_controller.rb
+++ app/controllers/admin/email_controller.rb
@@ -1,11 +1,11 @@
 class Admin::EmailController < Comfy::Admin::Cms::BaseController
   def index
     if params[:beta_testers]
+      @beta = true
+      @users = BetaTester.all
+    else
       @beta = false
       @users = User.all
-    else
-      @beta = true
-      @users = BetaTester.alls
     end
   end
 end
