24bf843dcb2221bd57aa658c3c03810d641fdb27
Michael Cowden <michael.cowden@gmail.com>
2018-06-09 23:53:13 -0400

    new screenshots

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def screenshot_tag(path)
+    path = "screenshots/#{path}"
+    content_tag("div", class: 'screenshot') do
+      link_to asset_path(path), target: '_blank' do
+        image_tag(path)
+      end
+    end
+  end
 end
