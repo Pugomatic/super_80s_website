eb06abc87c81834ddf9de4ef78c45148c7eff5f8
Michael Cowden <michael.cowden@gmail.com>
2018-09-16 17:34:53 -0400

    minor website fix

--- app/controllers/game/data_controller.rb
+++ app/controllers/game/data_controller.rb
@@ -1,7 +1,7 @@
 module Game
   class DataController < ApplicationController
     protect_from_forgery with: :null_session
-  
+
     def create
       player = Player.find_by(email: params[:email], uid: params[:uid])
       player.update_from_game! params
