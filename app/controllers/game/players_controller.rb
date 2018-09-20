f43fad01f1d5e3b83441ba810ca986e92749297c
Michael Cowden <michael.cowden@gmail.com>
2018-09-20 10:25:17 -0400

    debug

--- app/controllers/game/players_controller.rb
+++ app/controllers/game/players_controller.rb
@@ -7,6 +7,7 @@ module Game
         Player.from_game(params)
         head :ok
       else
+        Rails.logger.warn "#{params[:game_api_key]} != #{ENV['GAME_API_KEY']}"
         head :not_implemented
       end
     end
--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -1,4 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
 # Configure sensitive parameters which will be filtered from the log file.
-Rails.application.config.filter_parameters += [:password, :game_api_key, :uid]
+Rails.application.config.filter_parameters += [:password, :game_api_key, :uid, :fb_data]
