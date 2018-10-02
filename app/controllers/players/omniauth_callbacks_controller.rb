d4cc30c71d7ee21c10635776b2d7304f49e14c3c
Michael Cowden <michael.cowden@gmail.com>
2018-10-02 15:46:17 -0400

    fix

--- app/controllers/players/omniauth_callbacks_controller.rb
+++ app/controllers/players/omniauth_callbacks_controller.rb
@@ -4,8 +4,11 @@ module Players
       @player = Player.from_omniauth(request.env["omniauth.auth"])
 
       if @player.persisted?
-        sign_in_and_redirect @player, event: :authentication
-        set_flash_message(:notice, :success, kind: "Facebook") if is_navigational_format?
+        sign_in @player, event: :authentication
+
+        flash[:notice] = "Congratulations!  You will be notified when the competition begins."
+
+        redirect_to game_profile_path @player
       else
         session["devise.facebook_data"] = request.env["omniauth.auth"]
         redirect_to new_player_registration_url
