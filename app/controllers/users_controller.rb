0b5f0f3697ca48389da05dbd774d992e297a0075
Michael Cowden <michael.cowden@gmail.com>
2018-10-02 18:09:14 -0400

    fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,14 +7,18 @@ class UsersController < ApplicationController
 
     if user_params[:email] =~ /163\.com$/
       flash[:notice] = "Nothing to see here"
+      redirect_to '/'
+
     elsif @user.save
-      flash[:notice] = "Most excellent!  You will hear from us soon."
+      #flash[:notice] = "Most excellent!  You will hear from us soon."
+      flash[:notice] = "Most excellent!  Make sure to register for our beta competition"
+
       # referer_tracking_after_create(@user)
+      redirect_to '/game/beta_competition'
     else
       flash[:error] = "Totally bogus email address, bub..."
+      redirect_to '/'
     end
-
-    redirect_to '/'
   end
 
   def user_params
