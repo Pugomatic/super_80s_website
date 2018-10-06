f25bc5c64fd2aa2ab1f1036ed81f6853f54c2a4b
Michael Cowden <michael.cowden@gmail.com>
2018-10-06 18:11:17 -0400

    more fine print

--- app/controllers/beta_testers_controller.rb
+++
@@ -1,9 +0,0 @@
-class BetaTestersController < ApplicationController
-  layout 'simple'
-
-  def index
-    if params[:code] == '19.84.1' && params[:email].present?
-      BetaTester.create(email: params[:email], version: params[:code])
-    end
-  end
-end
