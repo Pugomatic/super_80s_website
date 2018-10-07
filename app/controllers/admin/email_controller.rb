93eaa42899c5c8650e693b755285bb6d337706a1
Michael Cowden <michael.cowden@gmail.com>
2018-10-07 11:16:12 -0400

    fix

--- app/controllers/admin/email_controller.rb
+++ app/controllers/admin/email_controller.rb
@@ -6,7 +6,7 @@ class Admin::EmailController < Comfy::Admin::Cms::BaseController
     else
       @beta = false
       @players = Player.all.order(created_at: :desc)
-      @users = User.all.order(created_at: :desc).reject {|u| @players.map(&:email).include?(u.email) }
+      @users = User.all.order(created_at: :desc).reject {|u| @players.map(&:email).map(&:downcase).include?(u.email.downcase) }
     end
   end
 end
