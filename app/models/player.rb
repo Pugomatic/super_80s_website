c1ea815264c7ce35ef8b8659f4b98b171505f378
Michael Cowden <michael.cowden@gmail.com>
2018-10-20 12:54:10 -0400

    fix

--- app/models/player.rb
+++ app/models/player.rb
@@ -64,7 +64,7 @@ class Player < ApplicationRecord
   end
 
   def self.top_level_board
-    Player.includes(top_completed_level: :player_levels).where('players.id = player_levels.player_id AND player_levels.level_id = players.top_completed_level_id AND players.leader = ?', true).order('levels.number DESC, player_levels.high_score DESC')
+    Player.includes(top_completed_level: :player_levels, player_totals: {}).where('players.id = player_levels.player_id AND player_levels.level_id = players.top_completed_level_id AND players.leader = ?', true).order('levels.number DESC, player_totals.high_score DESC')
   end
 
   def self.from_game(all_params)
