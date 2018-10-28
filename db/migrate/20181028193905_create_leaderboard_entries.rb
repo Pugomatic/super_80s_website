ead77376ebf6acf6374015b3a1392d328ec1e853
Michael Cowden <michael.cowden@gmail.com>
2018-10-28 16:38:04 -0400

    caching leaderboards

--- db/migrate/20181028193905_create_leaderboard_entries.rb
+++ db/migrate/20181028193905_create_leaderboard_entries.rb
@@ -6,13 +6,13 @@ class CreateLeaderboardEntries < ActiveRecord::Migration[5.2]
       t.references :level, foreign_key: true
       t.integer :value
     end
-    add_column  :leaderboards, :joiner_tables, :string, default: 'players'
+    add_column  :leaderboards, :joiner_tables, :string, default: 'player'
     add_column  :leaderboards, :sorting, :string, default: 'value DESC'
     add_column  :leaderboards, :timeframe, :string, default: 'game'
     add_column  :leaderboards, :metric, :string, default: 'high_score'
     add_column  :leaderboards, :locked, :boolean, default: false
     add_column  :leaderboards, :ending_at, :datetime
 
-    Leaderboard.last.update_attributes sorting: 'levels.number DESC, value DESC', joiner_tables: 'levels,players', metric: 'high_score', timeframe: 'game'
+    Leaderboard.last.update_attributes sorting: 'levels.number DESC, value DESC', joiner_tables: 'level player', metric: 'high_score', timeframe: 'game'
   end
 end
