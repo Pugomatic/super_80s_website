8d2abd98ef9bf930f504a7bee4968290a8266e22
Michael Cowden <michael.cowden@gmail.com>
2018-11-23 12:22:14 -0500

    lb

--- app/models/leaderboard_entry.rb
+++ app/models/leaderboard_entry.rb
@@ -4,6 +4,5 @@ class LeaderboardEntry < ApplicationRecord
   belongs_to  :level #optional
 
   validates :leaderboard, presence: true
-  validates :player, uniqueness: true, presence: true
-
+  validates :player, uniqueness: {scope: :leaderboard_id}, presence: true
 end
--- app/models/leaderboard_player.rb
+++ app/models/leaderboard_player.rb
@@ -1,4 +1,6 @@
 class LeaderboardPlayer < ApplicationRecord
   belongs_to :leaderboard
   belongs_to :player
+
+  validates :player_id, uniqueness: {scope: :leaderboard_id}
 end
