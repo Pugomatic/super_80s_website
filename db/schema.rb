4e7075e1b8ff67d27ecb6561f6650d7f7de2b628
Michael Cowden <michael.cowden@gmail.com>
2019-02-23 11:41:02 -0500

    fix margin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_27_235129) do
+ActiveRecord::Schema.define(version: 2019_01_28_015608) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
