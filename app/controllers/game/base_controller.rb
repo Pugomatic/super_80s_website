690e1502f4bd46966591dcffd9f40c4ac2aac2e2
Michael Cowden <michael.cowden@gmail.com>
2019-05-01 13:01:20 -0400

    fix

--- app/controllers/game/base_controller.rb
+++ app/controllers/game/base_controller.rb
@@ -5,16 +5,16 @@ module Game
     protected
 
     def set_paths
-      if @player || session[:player_id] || current_player
-        player = @player || session[:player_id] || current_player
+      if @player || session[:player_id]
+        player = @player || session[:player_id]
         if player.respond_to?(:handle)
           @player_name = player.handle
         else
           @player_name = Player.get(session[:player_id])&.handle
         end
 
-        @profile_path = current_player && game_profile_path(current_player)
-        @disco_path = (@player || current_player) && game_profile_discographies_path(@player || current_player)
+        @profile_path = game_profile_path(@player)
+        @disco_path = @player && game_profile_discographies_path(@player)
 
       else
         @profile_path = game_profiles_path
