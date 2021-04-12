e22f79cfa1a4fb4f3f1eb4162d68aa7771915b3c
Michael Cowden <mcowden@codeforamerica.org>
2021-04-12 09:20:02 -0400

    no key

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,5 +11,5 @@ Rails.application.configure do
   config.i18n.fallbacks = true
   config.active_support.deprecation = :notify
   config.log_formatter = ::Logger::Formatter.new
-  config.require_master_key = true
+  config.require_master_key = false
 end
