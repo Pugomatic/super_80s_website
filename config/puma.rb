48f7d7ae7786c2ea0b4c58d37dcc41bb826cba54
Michael Cowden <mcowden@codeforamerica.org>
2021-04-12 00:31:03 -0400

    log

--- config/puma.rb
+++ config/puma.rb
@@ -12,7 +12,7 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
 workers ENV.fetch("WEB_CONCURRENCY") { 2 }
-stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
+stdout_redirect "#{application_path}/log/puma.stdout.log", "#{application_path}/log/puma.stderr.log", true
 
 preload_app!
 
